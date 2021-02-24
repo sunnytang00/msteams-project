2ef04540b74a9f2ebbf4923965e094da82d1cf6c
Sunny Tang <sunny.tang@student.unsw.edu.au>
2021-02-24 11:32:14 +1100

    Removed pre-existing fields inside

--- src/data.py
+++ src/data.py
@@ -1,22 +1,8 @@
 data = {
     'users': [
-        {
-            'id': 1,
-            'name' : 'user1',
-        },
-        {
-            'id': 2,
-            'name' : 'user2',
-        },
+
     ],
     'channels': [
-        {
-            'id': 1,
-            'name' : 'channel1',
-        },
-        {
-            'id': 2,
-            'name' : 'channel2',
-        },
+
     ],
 }
