acf7881484a28aaac46b92daf16a24d462d9ead7
Wang Zhang <z5191535@ad.unsw.edu.au>
2021-02-26 23:19:21 +0800

    fixed test and finish function of channels_create

--- src/channels.py
+++ src/channels.py
@@ -1,3 +1,6 @@
+from src.data import data
+from src.error import InputError
+
 def channels_list_v1(auth_user_id):
     return {
         'channels': [
@@ -19,6 +22,13 @@ def channels_listall_v1(auth_user_id):
     }
 
 def channels_create_v1(auth_user_id, name, is_public):
+    #check if the name of channels is too long
+    global data
+    if len(name) > 20:
+        raise InputError('Name is too long')
+
+    new_channel_id = len(data['channels']) + 1
+    data['channels'].append({'id': new_channel_id, 'name': name, 'is_public': is_public})
     return {
-        'channel_id': 1,
+        'channel_id': new_channel_id,
     }
--- tests/channels_create_test.py
+++ tests/channels_create_test.py
@@ -1,18 +1,17 @@
 import pytest
-from src.channels import channels_create
+from src.channels import channels_create_v1
 from src.data import data
+from src.error import InputError
 
-def 
-
-def test_vaild_input:
+def test_vaild_input():
     channel_len = len(data['channels'])
     channel_id = channel_len + 1
-    assert channels_create_v1(channel_id, "correct", True) == {}
+    assert channels_create_v1(channel_id, "correct", True) == {'channel_id': channel_id}
 
-def test_name_length:
+def test_name_length():
     channel_len = len(data['channels'])
     channel_id = channel_len + 1
-    with pytest.rasie(InputError) as e: 
+    with pytest.raises(InputError) as e: 
         channels_create_v1(channel_id, "first channel" * 10, True)
         assert 'Name is too long' in str(e)
 
