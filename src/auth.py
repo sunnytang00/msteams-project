f3e9f1b4fac9593330e3421346fe23c14347967f
Amin Ghasembeigi <a.ghasembeigi@student.unsw.edu.au>
2021-03-01 22:07:53 +1100

    Add a docstring to src/auth.py and improve function docstring of src/auth.py auth_register_v1

--- src/auth.py
+++ src/auth.py
@@ -2,7 +2,14 @@ from src.data import data
 from src.error import InputError
 import re
 
+
+""" Register and login authentication.
+
+This module demonstrates user registration and login authentication as specified by the COMP1531 Major Project specification.
+"""
+
 regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+
 
 def auth_login_v1(email, password):
 
@@ -18,16 +25,18 @@ def auth_login_v1(email, password):
                 raise InputError('Password is not correct.')
         else:
             raise InputError('Email does not belong to a user.')
-        
-
-            
-  
-    
 
 def auth_register_v1(email, password, name_first, name_last):
-    """
-    Registers the user by appending them into the user list inside data.
-    Takes in 4 parameters, returns the user's newly created id.
+    """ Registers the user by appending them into the user list inside data.
+
+    Args:
+        email (str): The users email address.
+        password (str): The users password.
+        name_first (str): The users first name.
+        name_last (str): The users last name.
+
+    Returns:
+        dict: The newly created users user id.
     """
     global data
     user_id = len(data) + 1
