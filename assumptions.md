de5bb792946f7a13f7583f9738af48b903a8b48d
Wang Zhang <z5191535@ad.unsw.edu.au>
2021-03-02 11:30:51 +0800

    added invaildID test into channels_create_test, channel_listall_test finished

--- assumptions.md
+++ assumptions.md
@@ -1,2 +1,3 @@
 # List of assumptions
 * No dictionary in the users list will removed inside src/data.py
+* Name of channels should be distinct
--- tests/channels_create_test.py
+++ tests/channels_create_test.py
@@ -1,10 +1,15 @@
 import pytest
 from src.channels import channels_create_v1
 from src.data import data
-from src.error import InputError
+from src.error import InputError,AccessError
 from src.auth import auth_register_v1
 from src.other import clear_v1
 
+def test_invaild_userID():
+    clear_v1()
+    with pytest.raises(AccessError) as e: 
+        channels_create_v1("invaild id here", "first channel" * 10, True)
+        assert 'User ID is invaild' in str(e)
 def test_vaild_input():
     clear_v1()
     user_id = auth_register_v1('bobsmith2@gmail.com','12345678','bob','smith')
