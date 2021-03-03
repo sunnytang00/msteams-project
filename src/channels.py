e5f2ac90fbb0c8db3cbee2ee1a142d040c1425d2
Amin Ghasembeigi <a.ghasembeigi@student.unsw.edu.au>
2021-03-03 16:40:27 +1100

    Improve code and styling in channels functions

--- src/channels.py
+++ src/channels.py
@@ -1,10 +1,10 @@
 from src.data import data
-from src.error import InputError,AccessError
+from src.error import InputError, AccessError
 
 def channels_list_v1(auth_user_id):
     """ TODO: add docstring
     """
-    if (type(auth_user_id) != int or auth_user_id < 0):
+    if type(auth_user_id) != int or auth_user_id < 0:
         raise AccessError('User ID is invaild')
 
     if len(data['channels']) == 0:
@@ -12,7 +12,7 @@ def channels_list_v1(auth_user_id):
 
     channels_of_user = []
     for channels in data['channels']:
-        if channels['uesr_id'] == auth_user_id:
+        if channels['user_id'] == auth_user_id:
             channels_of_user.append(channels)
     
     return channels_of_user
@@ -20,42 +20,45 @@ def channels_list_v1(auth_user_id):
 def channels_listall_v1(auth_user_id):
     """ TODO: add docstring
     """
-    if (type(auth_user_id) != int or auth_user_id < 0):
+    if type(auth_user_id) != int or auth_user_id < 0:
         raise AccessError('User ID is invaild')
-
 
     return data['channels']
 
 def channels_create_v1(auth_user_id, name, is_public):
     """ TODO: add docstring
     """
-    #check if the name of channels is too long
     global data
 
-    if(type(auth_user_id) != int or auth_user_id < 0):
+    if type(auth_user_id) != int or auth_user_id < 0:
         raise AccessError('User ID is invaild')
 
+    # check if the name of channels is too long
     if len(name) > 20:
         raise InputError('Name is too long')
 
     found_user = False
-    found_duplicate_name = False
     for user in data['users']:
         if user['id'] == auth_user_id:
             found_user = True
             break
+
+    if not found_user:
+       raise InputError(f'User with id {auth_user_id} does not exist')  
     
     for channel in data['channels']:
-        if len(data['channels']) > 0:
-            if channel['name'] == name:
-                found_duplicate_name = True
-                break
-    if found_user == False:
-       raise InputError(f'User with id {auth_user_id} does not exist')  
-    if found_duplicate_name == True:
-        raise InputError(f'Channel with name {name} already exists')  
+        if channel['name'] == name:
+            raise InputError(f'Channel with name {name} already exists')  
+
     new_channel_id = len(data['channels']) + 1
-    data['channels'].append({'id': new_channel_id, 'name': name, 'uesr_id': auth_user_id, 'is_public': is_public})
+
+    data['channels'].append({
+        'id': new_channel_id,
+        'name': name,
+        'user_id': auth_user_id,
+        'is_public': is_public
+    })
+
     return {
         'channel_id': new_channel_id,
     }
--- tests/channels_create_test.py
+++ tests/channels_create_test.py
@@ -1,7 +1,6 @@
 import pytest
 from src.channels import channels_create_v1
-from src.data import data
-from src.error import InputError,AccessError
+from src.error import InputError, AccessError
 from src.auth import auth_register_v1
 from src.other import clear_v1
 
@@ -10,12 +9,16 @@ def test_invaild_userID():
     with pytest.raises(AccessError) as e: 
         channels_create_v1("invaild id here", "first channel" * 10, True)
         assert 'User ID is invaild' in str(e)
+
 def test_vaild_input():
     clear_v1()
-    user_id = auth_register_v1('bobsmith2@gmail.com','12345678','bob','smith')
-    channel_len = len(data['channels'])
-    channel_id = channel_len + 1
-    assert channels_create_v1(user_id['auth_user_id'], "correct", True) == {'channel_id': channel_id}
+    user_id = auth_register_v1(email='bobsmith2@gmail.com',
+                                password='12345678',
+                                name_first='bob',
+                                name_last='smith')
+    channel_id = 1
+    result = channels_create_v1(user_id['auth_user_id'], "correct", True)
+    assert result == {'channel_id': channel_id} 
 
 def test_name_length():
     clear_v1()
@@ -26,10 +29,10 @@ def test_name_length():
 
 def test_userID_not_exist():
     clear_v1()
-    invaild_id = 10
+    invalid_id = 10
     with pytest.raises(InputError) as e: 
-        channels_create_v1(invaild_id, "second channel", True)
-        assert f'User with id {auth_user_id} does not exist' in str(e)
+        channels_create_v1(invalid_id, "second channel", True)
+        assert f'User with id {invalid_id} does not exist' in str(e)
 
 def test_duplicate_channel_name():
     clear_v1()
@@ -39,4 +42,3 @@ def test_duplicate_channel_name():
     with pytest.raises(InputError) as e: 
         channels_create_v1(user_id['auth_user_id'], channel_name, True)
         assert f'Channel with name {channel_name} already exists' in str(e)
-
