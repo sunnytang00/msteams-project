8f49ae57730ae1881a348bb96510df0c6c463b8b
Amin Ghasembeigi <a.ghasembeigi@student.unsw.edu.au>
2021-03-03 21:09:29 +1100

    Overhaul testing with helper functions and fix bugs in tests/helper.py

--- tests/channels_listall_test.py
+++ tests/channels_listall_test.py
@@ -1,9 +1,9 @@
 import pytest
 from src.channels import channels_listall_v1,channels_create_v1
-from src.data import data
 from src.error import InputError,AccessError
 from src.auth import auth_register_v1
 from src.other import clear_v1
+from .helper import helper
 
 def test_invaild_userID():
     clear_v1()
@@ -11,12 +11,13 @@ def test_invaild_userID():
         channels_listall_v1("invaild id here")
         assert 'User ID is invaild' in str(e)
 
-def test_vaild_input():
+def test_vaild_input(helper):
     clear_v1()
+    helper.register_users(10)
+    helper.create_channels(10)
     user_id = auth_register_v1('bobsmith2@gmail.com','12345678','bob','smith')
     channels_create_v1(user_id['auth_user_id'], "correct", True)
     channels_create_v1(user_id['auth_user_id'], "correct2", True)
     channels_create_v1(user_id['auth_user_id'], "correct3", True)
-    assert len(channels_listall_v1(user_id['auth_user_id'])) == 3
 
- 
+    assert len(channels_listall_v1(user_id['auth_user_id'])) == 13
