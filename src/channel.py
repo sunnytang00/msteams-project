446b0def401a40b32ae32a7c1ea5f6a733c1a04b
Wang Zhang <wang.zhang1@student.unsw.edu.au>
2021-03-04 12:11:59 +1100

    finished channel_join, added more tests for it, modified channels_list

--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,7 @@
+from src.data import data
+from src.error import InputError, AccessError
+
+
 def channel_invite_v1(auth_user_id, channel_id, u_id):
     """ TODO: add docstring
     """
@@ -46,8 +50,50 @@ def channel_leave_v1(auth_user_id, channel_id):
     }
 
 def channel_join_v1(auth_user_id, channel_id):
-    """ TODO: add docstring
+    """ 
+    Add user as the member of channel with specified ID
+
+    Arguments: 
+        auth_user_id (int) - ID of authorised user
+        channel_id (int) - ID of the channel
+
+    Exceptions:
+        AccessError - Occurs when the auth_user_id is invalid
+        AccessError - Occurs when the channel is private 
+        InputError - Occurs when the channel_id is invalid
+        InputError - Occurs when the channel with id entered is not created
+        InputError - Occurs when the channel is private and the user is not owner of it
+
+    Return Value:
+        Return nothing
     """
+    global data
+
+    if type(auth_user_id) != int or auth_user_id < 0:
+        raise AccessError('User ID is invaild')
+
+    if type(channel_id) != int or channel_id < 0:
+        raise InputError('Channel ID is invaild')
+
+    found_channel = False
+    channels = data['channels']
+
+    for channel in channels:
+        if channel['id'] == channel_id:
+            found_channel = True
+            if not channel['is_public']: 
+                raise AccessError('Cannot access the private channel')
+            if channel['member'].count(auth_user_id) > 0:
+                raise InputError('The user is already in the channel')
+            
+            channel['member'].append(auth_user_id)
+            break
+
+    if not found_channel:
+        raise InputError('Channel with ID {channel_id} does not exist')
+    
+    
+
     return {
     }
 
--- src/channels.py
+++ src/channels.py
@@ -1,24 +1,50 @@
 from src.data import data
 from src.error import InputError, AccessError
+"""
+Create and show the list of channels
+
+This module demonstrates creation of channels and ability of showing list of channels with associated details
+as specified by the COMP1531 Major Project specification.
+"""
 
 def channels_list_v1(auth_user_id):
-    """ TODO: add docstring
+    """ 
+    Shows the list of channels and the associated details that authorised user is part of
+
+    Arguments: 
+        auth_user_id (int) - ID of authorised user
+
+    Exceptions:
+        AccessError - Occurs when the auth_user_id is invalid 
+
+    Return Value:
+        channels_of_user (list) - a list contains channels that the user is part of 
     """
     if type(auth_user_id) != int or auth_user_id < 0:
         raise AccessError('User ID is invaild')
 
     if len(data['channels']) == 0:
-        return {}
+        return []
 
     channels_of_user = []
     for channels in data['channels']:
-        if channels['user_id'] == auth_user_id:
+        if channels['member'].count(auth_user_id) > 0:
             channels_of_user.append(channels)
     
     return channels_of_user
 
 def channels_listall_v1(auth_user_id):
-    """ TODO: add docstring
+    """ 
+    Shows all the channels on the list and their details
+    
+    Arguments: 
+        auth_user_id (int) - ID of authorised user
+
+    Exceptions:
+        AccessError - Occurs when the auth_user_id is invalid 
+
+    Return Value:
+        data['channels'] (list) - A list contains all the channels stored in the storage
     """
     if type(auth_user_id) != int or auth_user_id < 0:
         raise AccessError('User ID is invaild')
@@ -26,7 +52,22 @@ def channels_listall_v1(auth_user_id):
     return data['channels']
 
 def channels_create_v1(auth_user_id, name, is_public):
-    """ TODO: add docstring
+    """ 
+    Create a public/private channel with specified name and owned by user with specified ID
+        
+    Arguments: 
+        auth_user_id (int) - ID of authorised user
+        name (str) - Name for the new channel
+        is_public (bool) - Flags that indicate the channel is public or private
+
+    Exceptions:
+        AccessError - Occurs when the auth_user_id is invalid 
+        InputError - Occurs when the length of name is too long
+        InputError - Occurs when the user with auth_user_id is not registered
+        InputError - Occurs when the channel with specified name already exists
+
+    Return Value:
+        data['channels'] (list) - A list contains all the channels stored in the storage
     """
     global data
 
@@ -56,6 +97,7 @@ def channels_create_v1(auth_user_id, name, is_public):
         'id': new_channel_id,
         'name': name,
         'user_id': auth_user_id,
+        'member' : [auth_user_id],
         'is_public': is_public
     })
 
