547c08cab098c6d6753ae4fbcc70159938471d93
aminbeigi <me@aminbeigi.com>
2021-03-07 14:09:34 +1100

    Add test_user_not_authorised

--- tests/channel_details_test.py
+++ tests/channel_details_test.py
@@ -36,3 +36,24 @@ def test_invalid_channel_id():
         assert f'Channel ID {invalid_channel_id} is not a valid channel.' in str(e)
 
 # TODO: AccessError exception test
+def test_user_not_authorised():
+    """Authorised user is not a member of channel with channel_id"""
+    clear_v1()
+    user = auth_register_v1(email='bobsmith@gmail.com',
+                                password='FVn4HTWEsz8k6Msf',
+                                name_first='Bob',
+                                name_last='Smith')
+    user_id = user['auth_user_id']
+
+    channel_owner_user = auth_register_v1(email='harrypotter7@gmail.com',
+                                            password='qw3rtyAppl3s@99',
+                                            name_first='Harry',
+                                            name_last='Potter')
+    channel_owner_user_id = channel_owner_user['auth_user_id']    
+
+    channel = channels_create_v1(channel_owner_user_id, 'Cat Society', True)
+    channel_id = channel['channel_id']
+
+    with pytest.raises(InputError) as e: 
+        channel_details_v1(auth_user_id=user_id, channel_id=channel_id)
+        assert f'Authorised user {user_id} is not a member of channel with channel_id' in str(e)
