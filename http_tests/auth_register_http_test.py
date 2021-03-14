4b4a121bed44ed0bba04371a23541a675aa38b70
Sunny <z5255035@ad.unsw.edu.au>
2021-03-15 01:34:21 +1100

    added basic auth_register_http test

--- http_tests/auth_register_http_test.py
+++ http_tests/auth_register_http_test.py
@@ -1,20 +1,21 @@
-from src.http.auth_register_http import register
 import requests
 from json import loads
 from src.base.config import url
+from src.server import register
 
 def test_register_basic():
-
-    out = requests.post(url + "auth/register/v2", json = {
-        'email' : 'harrypotter@gmail.com'
-        'password' : 'dumbledore'
-        'name_first' : 'harry'
+    
+    r = requests.post(url + 'auth/register/v2', json = {
+        'email' : 'harrypotter@gmail.com',
+        'password' : 'dumbledore',
+        'name_first' : 'harry',
         'name_last' : 'potter'
     })
 
-    output = out.json()
+    output = r.json()
 
     #may need to assert what token is = to? apparently atm should be user_id
     #but not sure
 
-    assert output['auth_user_id'] == 1
+    assert output['auth_user_id'] == {'auth_user_id' : 1}
+ 
