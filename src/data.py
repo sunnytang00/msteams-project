718ae01a3a71f2dd6344e78e347b0db01ba5cce8
Sarth Bishnoi <z5287670@ad.unsw.edu.au>
2021-03-15 16:29:43 +1100

    Added dummy data

--- src/data.py
+++ src/data.py
@@ -6,3 +6,50 @@ data = {
 
     ],
 }
+
+#add token?
+''' 
+Dummy Data for Database
+
+data = {
+    'users': [
+        { 
+        'u_id': 0, #user_id,
+        'email': sample_0@something.com                         #email,
+        'name_first': first_sample_name_0                       #name_first,
+        'name_last': last_sample_name_0                         #name_last,
+        'handle_str': first_sample_name_0last_sample_name_0     #handle_str,
+        'password': password_0_1324&#!$                            #password
+        },
+        { 
+        'u_id': 1, #user_id,
+        'email': sample_1@something.com                         #email,
+        'name_first': first_sample_name_1                       #name_first,
+        'name_last': last_sample_name_1                         #name_last,
+        'handle_str': first_sample_name_1last_sample_name_1     #handle_str,
+        'password': password_1_1324&#!$                            #password
+        },
+    ],
+    'channels': [           
+        {
+        'channel_id': 0,
+        'name': Sample_channel_0,
+        'owner_members': [
+            0,
+        ],
+        'all_members': [
+            0,
+            1,
+        ],
+        'messages': [
+            {
+                'message_id': 0,
+                'u_id': 0,
+                'message': 'Hello world',
+                'time_created': 1582426789,
+            }
+        ],
+        'is_public': TRUE
+        }
+    ],
+}
