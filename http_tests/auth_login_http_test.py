f761a14e7c0b1054e24c6585b6b54b9e798a6816
Sunny <z5255035@ad.unsw.edu.au>
2021-03-15 21:44:52 +1100

    added tests for auth_login http function

--- http_tests/auth_login_http_test.py
+++ http_tests/auth_login_http_test.py
@@ -6,26 +6,22 @@ from src.base.config import url
 
 
 def test_login_basic_http():
-    """
 
-    out = requests.post(url + 'auth/register/v2', json = {
+    r = requests.post(url + 'auth/register/v2', json = {
         'email' : 'harrypotter@gmail.com',
         'password' : 'dumbledore',
         'name_first' : 'harry',
         'name_last' : 'potter'
     })
 
-    output = register_http()
-    out1 = output.json()
+    output = r.json()
 
-    r = requests.post(url + 'auth/login/v2', json = {
+    out = requests.post(url + 'auth/login/v2', json = {
         'email' : 'harrypotter@gmail.com',
         'password' : 'dumbledore'
     })
 
-    r1 = login_http()
-    r_output = r1.json()
+    expected = out.json()
 
-    assert r_output['auth_user_id'] == out1['auth_user_id']
-    """
-    pass
+    assert output['auth_user_id'] == expected['auth_user_id']
+    
