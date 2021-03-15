782822592ffa1e764a0b544f2bc40039f777da41
Sunny <z5255035@ad.unsw.edu.au>
2021-03-15 22:19:46 +1100

    changed code for test function

--- http_tests/auth_login_http_test.py
+++ http_tests/auth_login_http_test.py
@@ -1,27 +1,27 @@
-#from src.server import login
-from src.server import register
 import requests
 from json import loads
 from src.base.config import url
 
 
 def test_login_basic_http():
-
+    
     r = requests.post(url + 'auth/register/v2', json = {
-        'email' : 'harrypotter@gmail.com',
-        'password' : 'dumbledore',
-        'name_first' : 'harry',
-        'name_last' : 'potter'
+        'email' : 'harrypotter@gmail1.com',
+        'password' : 'dumbledore1',
+        'name_first' : 'harry1',
+        'name_last' : 'potter1'
     })
 
     output = r.json()
 
     out = requests.post(url + 'auth/login/v2', json = {
-        'email' : 'harrypotter@gmail.com',
-        'password' : 'dumbledore'
+        'email' : 'harrypotter@gmail1.com',
+        'password' : 'dumbledore1'
     })
 
     expected = out.json()
 
     assert output['auth_user_id'] == expected['auth_user_id']
+    
+    
     
