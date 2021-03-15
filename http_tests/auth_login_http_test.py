f4d278daf3884889b56aa6bc6633cdef682335eb
aminbeigi <me@aminbeigi.com>
2021-03-16 09:44:45 +1100

    Add status code tests

--- http_tests/auth_login_http_test.py
+++ http_tests/auth_login_http_test.py
@@ -4,6 +4,7 @@ from src.base.config import url
 
 
 def test_login_basic_http():
+    ### register
     response = requests.post(url + 'auth/register/v2', json = {
         'email' : 'harrypotter@gmail1.com',
         'password' : 'dumbledore1',
@@ -12,15 +13,20 @@ def test_login_basic_http():
     })
 
     status_code = response.status_code
-    # reading data from response
-    register_data = response.json()
+    assert status_code == 201
 
-    login_data = requests.post(url + 'auth/login/v2', json = {
+    register_data = response.json()
+    registered_auth_user_id = register_data.get('auth_user_id')
+
+    ### login user
+    response = requests.post(url + 'auth/login/v2', json = {
         'email' : 'harrypotter@gmail1.com',
         'password' : 'dumbledore1'
     })
-
-    expected = login_data.json()
-
-    assert register_data == expected
+    status_code = response.status_code
     assert status_code == 200 
+
+    login_data = response.json()
+    expected_auth_user_id = login_data.get('auth_user_id')
+
+    assert registered_auth_user_id == expected_auth_user_id
