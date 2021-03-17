4b8f4d2a99d560e6e8b3c69574d5508e9763d770
Sunny <z5255035@ad.unsw.edu.au>
2021-03-17 15:52:56 +1100

    added jwt encoding and decoding functions to helper.py

--- src/routes/helper.py
+++ src/routes/helper.py
@@ -1,4 +1,13 @@
 import hashlib
+import jtw
 
 def sha256_hash(password: str) -> str:
     return hashlib.sha256(password.encode()).hexdigest()
+
+#Takes in 2 strings, first name and last name, concatenates them and jwts it
+def encode_jwt(firstname: str, lastname: str)  -> str:
+    ret = lower(firstname) + lower(lastname)
+    return jwt.encode(ret, "FRI09BECHO", algorithms='HS256')
+#takes in a jwt and decodes it
+def decode_jwt(jwt: str) -> str:
+    return jwt.decode(jwt, "FRI09BECHO", algorithms='HS256')
