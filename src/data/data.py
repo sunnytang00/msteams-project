bea71e381fe8f09a3b7580dbf1cf0c470ff072c2
aminbeigi <me@aminbeigi.com>
2021-03-17 17:22:02 +1100

    Properly update dict

--- src/data/data.py
+++ src/data/data.py
@@ -10,8 +10,11 @@ data = {
 }
 
 try:
+    # update dict with stored data
     with open('store.json', 'r') as f:
-        data = load(f)
+        stored_data = load(f)
+        data['users'] = stored_data.get('users')
+        data['channels'] = stored_data.get('channels')
 except FileNotFoundError:
     # initialise file
     with open('store.json', 'w') as f:
