5e71b266bb68831eb8a260711b45e39f0c9d1517
Sunny <z5255035@ad.unsw.edu.au>
2021-03-17 16:16:24 +1100

    added checking for whether handle already exists

--- src/routes/helper.py
+++ src/routes/helper.py
@@ -5,9 +5,12 @@ def sha256_hash(password: str) -> str:
     return hashlib.sha256(password.encode()).hexdigest()
 
 #Takes in 2 strings, first name and last name, concatenates them and jwts it
-def encode_jwt(firstname: str, lastname: str)  -> str:
-
-    ret = firstname.lower() + lastname.lower()
+#if handle is taken
+def encode_jwt(firstname: str, lastname: str, num: int or None) -> str:
+    if num != None:
+        ret = firstname.lower() + lastname.lower() + num
+    else:
+        ret = firstname.lower() + lastname.lower()
     return jwt.encode(ret, "FRI09BECHO", algorithm='HS256')
 
 #takes in a jwt and decodes it
