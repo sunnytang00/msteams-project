d1c5f554f1f01f4189850bf107c0714e66a3088a
aminbeigi <me@aminbeigi.com>
2021-03-20 09:37:17 +1100

    Update global variable with content inside data.json

--- src/data/data.py
+++ src/data/data.py
@@ -1,3 +1,5 @@
+from json import load, JSONDecodeError
+
 data = {
     'users': [
 
@@ -6,3 +8,17 @@ data = {
 
     ],
 }
+
+try:
+    # update dict with stored data
+    with open('src/store.json', 'r') as f:
+        stored_data = load(f)
+
+    with open('src/store.json', 'w') as f:
+        data['users'] = stored_data.get('users')
+        data['channels'] = stored_data.get('channels')
+
+except (FileNotFoundError, JSONDecodeError) as e:
+    # initialise file
+    with open('src/store.json', 'w') as f:
+        pass
