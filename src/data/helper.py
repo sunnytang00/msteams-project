4c8848bd014ab0d4b0b608a60b66d0d8b64ede8b
aminbeigi <me@aminbeigi.com>
2021-03-22 14:17:11 +1100

    Add update_name_first and update_name_last

--- src/data/helper.py
+++ src/data/helper.py
@@ -19,37 +19,14 @@ def save_data(func):
     return wrapper
 '''
 
-def get_users() -> list:
-    """Get list of user from data storage
-    
-    Arguments:
-        This function takes no argument
+def clear_data() -> None:
+    cleared_data = {
+        "users": [],
+        "channels": []
+    }
 
-    Returns:
-        users (list): List of users
-    """
-    with open(data_path, 'r') as f:
-        data = json.load(f)
-
-    users = data['users']
-
-    return users
-
-def get_channels() -> list:
-    """Get list of channel from data storage
-    
-    Arguments:
-        This function takes no argument
-
-    Returns:
-        channels (list): List of channels
-    """
-
-    with open(data_path, 'r') as f:
-        data = json.load(f)
-
-    channels = data["channels"]
-    return channels
+    with open(data_path, 'w') as f:
+        json.dump(cleared_data, f)
 
 def get_data() -> dict:
     """Get data stored on data storage
@@ -65,19 +42,51 @@ def get_data() -> dict:
 
     return data
 
-def store_user(user: dict) -> bool:
-    """Store the data of user on data storage
+
+def get_users() -> list:
+    """Get list of user from data storage
     
     Arguments:
-        user (list): List of users
+        This function takes no argument
 
     Returns:
-        True if the user data stored successfully
-        False if fail to store user data
+        users (list): List of users
+    """
+    # TODO: make getters one liners.
+    data = get_data()
+
+    users = data.get('users')
+
+    return users
+
+def get_channels() -> list:
+    """Get list of channel from data storage
+    
+    Arguments:
+        This function takes no argument
+
+    Returns:
+        channels (list): List of channels
+    """
+    data = get_data()
+
+    channels = data.get("channels")
+    return channels
+
+
+def store_user(user: dict) -> bool:
+    """store the data of user on data storage
+    
+    arguments:
+        user (list): list of users
+
+    returns:
+        true if the user data stored successfully
+        false if fail to store user data
     """
     data = get_data()
     
-    data["users"] = user
+    data["users"].append(user)
 
     with open(data_path, 'w') as f:
         json.dump(data, f)
@@ -85,6 +94,24 @@ def store_user(user: dict) -> bool:
     if get_users() == data["users"]:
         return True
     return False
+
+def update_name_first(u_id: int, name_first: str) -> bool:
+    """TODO"""
+    data = get_data()
+
+    data["users"][u_id-1]['name_first'] = name_first
+
+    with open(data_path, 'w') as f:
+        json.dump(data, f)
+
+def update_name_last(u_id: int, name_last: str) -> bool:
+    """TODO"""
+    data = get_data()
+
+    data["users"][u_id-1]['name_last'] = name_last
+
+    with open(data_path, 'w') as f:
+        json.dump(data, f)
 
 def store_channel(channel: list) -> bool:
     """Store the data of channel on data storage
@@ -98,7 +125,7 @@ def store_channel(channel: list) -> bool:
     """
     data = get_data()
 
-    data["channels"] = channel
+    data["channels"].append(channel)
 
     with open(data_path, 'w') as f:
         json.dump(data, f)
@@ -106,3 +133,12 @@ def store_channel(channel: list) -> bool:
     if get_channels() == data["channels"]:
         return True
     return False
+
+def append_channel_all_members(channel_id: int, u_id: int) -> None:
+    """TODO"""
+    data = get_data()
+
+    data["channels"][channel_id-1]['all_members'].append(u_id)
+
+    with open(data_path, 'w') as f:
+        json.dump(data, f)
