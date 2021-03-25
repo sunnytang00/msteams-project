381640efe081f6005f21bc1c60fcd4ff395715b3
Sarth Bishnoi <s.bishnoi@student.unsw.edu.au>
2021-03-26 10:09:25 +1100

    Update data.py

--- src/data/data.py
+++ src/data/data.py
@@ -1,30 +1,3 @@
-"""
-data = {
-    'users': [
-
-    ],
-    'channels': [
-
-    ],
-}
-"""
-
-"""
-try:
-    # update dict with stored data
-    with open(data_path, 'r') as f:
-        stored_data = load(f)
-
-    with open(data_path, 'w') as f:
-        data['users'] = stored_data.get('users')
-        data['channels'] = stored_data.get('channels')
-
-except (FileNotFoundError, JSONDecodeError) as e:
-    # initialise file
-    with open(data_path, 'w') as f:
-        pass
-
-"""
 """ 
 Dummy Data for Database
 
