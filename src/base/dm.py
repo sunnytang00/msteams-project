123e39e77e0b627e9a036ed82d638b1c3d11724e
aminbeigi <me@aminbeigi.com>
2021-03-30 17:16:43 +1100

    Add stub function for dm_create

---
+++ src/base/dm.py
@@ -0,0 +1,13 @@
+"""TODO"""
+
+#from src.base.error import InputError
+#from src.base.helper import 
+#from src.data.helper import 
+
+def dm_create(u_ids):
+    """TODO"""
+
+    return {
+        'dm_id': 1,
+        'dm_name': "testing"
+    }
