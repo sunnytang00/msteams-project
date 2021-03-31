5744bc309677e7c1c373152802deeeb5bb097114
Sunny Tang <sunny.tang@student.unsw.edu.au>
2021-04-01 00:23:55 +1100

    Add dm invite tests + code +helper

--- src/base/dm.py
+++ src/base/dm.py
@@ -2,7 +2,7 @@
 
 from src.base.error import InputError, AccessError
 from src.base.helper import get_dm_name, get_current_user, get_dm, user_is_dm_member, get_user
-from src.data.helper import get_dm_count, store_dm, get_dms, update_dm_list
+from src.data.helper import get_dm_count, store_dm, get_dms, update_dm_list, get_users, update_dm_users
 def dm_create(auth_user_id, u_ids):
     """TODO"""
     #using auth user in place of token
@@ -55,7 +55,22 @@ def dm_details_v1(auth_user_id, dm_id):
 
 #def dm_leave_v1(u_id, dm_id):
 
-#def dm_invite_v1(auth_user_id, dm_id, u_id):
+def dm_invite_v1(auth_user_id, dm_id, u_id):
+    if not get_dm(dm_id):
+        raise InputError(f"dm_id {dm_id} does not refer to a valid dm")
+
+    #consider case where u_id is already in u_ids???
+    if not get_current_user(u_id):
+        raise InputError(f"u_id {auth_user_id} does not refer to a valid user")
+
+    if not user_is_dm_member(dm_id, auth_user_id):
+        raise AccessError(f'user with user_id {auth_user_id} is not part of the dm')
+
+    dm_users = get_dm(dm_id).get('u_ids')
+    dm_users.append(u_id)
+    update_dm_users(dm_users, dm_id)
+
+
 
 def dm_remove_v1(auth_user_id, dm_id):
     if not get_dm(dm_id):
