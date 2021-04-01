adf896f0ccad5ca43cc3ab881fb39a5f7b56a3e8
aminbeigi <me@aminbeigi.com>
2021-04-01 18:42:13 +1100

    Add test

---
+++ tests/message_remove_test.py
@@ -0,0 +1,29 @@
+import pytest
+from src.base.message import message_send_v1, message_remove_v1
+from src.base.auth import auth_register_v1
+from src.base.other import clear_v1
+from src.base.error import InputError, AccessError
+from src.data.helper import get_message_count
+from tests.helper import helper, clear
+from src.base.channels import channels_create_v1
+
+@clear
+def test_message_remove_single():
+    user = auth_register_v1(email='harrypotter@gmail.com',
+                        password='qw3rtyAppl3s@99',
+                        name_first='Harry',
+                        name_last='Potter')
+
+    auth_user_id = user['auth_user_id']
+    assert auth_user_id == 1
+
+    channel_id = channels_create_v1(auth_user_id, "message_test", True).get('channel_id')
+    assert channel_id == 1
+
+    message_info = message_send_v1(auth_user_id, channel_id, "an epic message")
+    message_id = message_info.get('message_id')
+    assert message_info.get('message_id') == 1
+    assert get_message_count() == 1
+
+    message_remove_v1(auth_register_v1, message_id)
+    assert get_message_count() == 0
