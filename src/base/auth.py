50c6cd98610fb4c386cdc1c84e5f498f12f2e7a5
aminbeigi <me@aminbeigi.com>
2021-04-02 14:27:26 +1100

    Remove redundant comments

--- src/base/auth.py
+++ src/base/auth.py
@@ -29,9 +29,7 @@ def auth_login_v1(email, password):
 
     # TODO: make helper function that returns user based on email
     for user in get_users():
-        # check user exists
         if email == user['email']:
-            # check corret password
             if password == user['password']:
                 return {'auth_user_id': user['u_id']}
             else:
