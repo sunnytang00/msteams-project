afadae8020cc365e05cafc3e7867e54a47fa314a
Wang Zhang <z5191535@ad.unsw.edu.au>
2021-04-04 17:01:42 +0800

    wrote http route for channels_list

--- http_tests/channels_listall_http_test.py
+++ http_tests/channels_listall_http_test.py
@@ -7,9 +7,32 @@ from src.base.helper import token_to_auth_user_id
 @clear
 def test_valid_input(helper):
     user1 = helper.register_user(1)
-    helper.register_uesr(2)
     
     token1 = user1.json().get('token')
+    assert token1
+
+    requests.post(url + 'channels/create/v2', json = {
+        'token': token1,
+        'name': 'channel_test1',
+        'is_public': True
+    })
+    requests.post(url + 'channels/create/v2', json = {
+        'token': token1,
+        'name': 'channel_test2',
+        'is_public': True
+    })
+    
+    channels = requests.get(url + 'channels/listall/v2?token=' + token1).json()
+
+    assert len(channels['channels']) == 2
+
+@clear
+def test_private_channel_exists(helper):
+    token1 = helper.register_user(1).json().get('token')
+    token2 = helper.register_user(2).json().get('token')
+
+    assert token1
+    assert token2
 
     ch1 = requests.post(url + 'channels/create/v2', json = {
         'token': token1,
@@ -19,24 +42,13 @@ def test_valid_input(helper):
     ch2 = requests.post(url + 'channels/create/v2', json = {
         'token': token1,
         'name': 'channel_test2',
-        'is_public': True
+        'is_public': False
     })
     ch1_id = ch1.json().get('channel_id')
     ch2_id = ch2.json().get('channel_id')
-    
-    channels = requests.get(url + 'channels/listall/v2?token=' + token1).json()
 
-    assert len(channels) == 2
-'''
-@clear
-def test_invalid_email():
-    # TODO:not sure if this is exactly right gotta check
-    response = requests.post(url + 'auth/register/v2', json = {
-        'email' : 'harrypotter_is_cool',
-        'password' : 'dubledore',
-        'name_first' : 'harry',
-        'name_last' : 'potter'
-    })
+    channels = requests.get(url + 'channels/listall/v2?token=' + token2).json()
 
-    assert response.status_code == 400
-'''
+    ch_ids = [channel['channel_id'] for channel in channels['channels']]
+    assert ch1_id in ch_ids and ch2_id not in ch_ids
+
