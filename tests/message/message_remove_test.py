03b2127e61ebc72c56e909627c0d3fbf5e7eba89
aminbeigi <me@aminbeigi.com>
2021-04-04 14:42:09 +1000

    Make tests better

--- tests/message/message_remove_test.py
+++ tests/message/message_remove_test.py
@@ -8,14 +8,9 @@ from tests.helper import helper, clear
 from src.base.channels import channels_create_v1
 
 @clear
-def test_message_remove_single():
-    """TODO test sucks """
-    user = auth_register_v1(email='harrypotter@gmail.com',
-                        password='qw3rtyAppl3s@99',
-                        name_first='Harry',
-                        name_last='Potter')
-
-    auth_user_id = user['auth_user_id']
+def test_message_remove_single(helper):
+    """TODO"""
+    auth_user_id = helper.register_user(1)
     assert auth_user_id == 1
 
     channel_id = channels_create_v1(auth_user_id, "message_test", True).get('channel_id')
@@ -25,21 +20,20 @@ def test_message_remove_single():
     message_id = message_info.get('message_id')
     assert message_info.get('message_id') == 1
 
-    start_and_end_keys = 2
-    assert len(channel_messages_v1(auth_user_id, channel_id, 1)) - start_and_end_keys == 1
+    messages = channel_messages_v1(auth_user_id, channel_id, 1).get('messages')
+    assert len(messages) == 1
 
-    message_remove_v1(auth_register_v1, message_id)
+    message_remove_v1(auth_user_id, message_id)
+
+    with pytest.raises(InputError) as e:
+        messages = channel_messages_v1(auth_user_id, channel_id, 1).get('messages')
+    assert "Start 1 is greater than the total number of messages in the channel" in str(e.value)
 
 
 @clear
-def test_message_no_longer_exists():
-    """TODO test sucks """
-    user = auth_register_v1(email='harrypotter@gmail.com',
-                        password='qw3rtyAppl3s@99',
-                        name_first='Harry',
-                        name_last='Potter')
-
-    auth_user_id = user['auth_user_id']
+def test_message_no_longer_exists(helper):
+    """TODO"""
+    auth_user_id = helper.register_user(1)
     assert auth_user_id == 1
 
     channel_id = channels_create_v1(auth_user_id, "message_test", True).get('channel_id')
