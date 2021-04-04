0f1f1001f1481a6a7b87c9a16cd221910577e5ca
Sunny <z5255035@ad.unsw.edu.au>
2021-04-04 17:18:56 +1000

    added tests for dm_create route

---
+++ http_tests/dm_create_http_test.py
@@ -0,0 +1,34 @@
+import requests
+from json import loads
+from src.config import url
+from http_tests.helper import clear, helper
+from src.base.helper import get_dm_name
+
+@clear
+
+def test_basic(helper):
+
+    response = helper.register_user(1)
+    response2 = helper.register_user(2)
+    response3 = helper.register_user(3)
+
+    data = response.json()
+    data2 = response2.json()
+    data3 = response3.json()
+
+    token = data.get('token')
+
+    u_id2 = data2.get('auth_user_id')
+    u_id3 = data3.get('auth_user_id')
+
+    dm = requests.post(url + 'dm/create/v1', json = {
+        'token' : token,
+        'u_ids' : [u_id2, u_id3]
+    })
+
+    dm_info = dm.json()
+    assert dm_info.get('dm_id') == 1
+    assert dm_info.get('dm_name') == get_dm_name([u_id2, u_id3])
+
+
+
