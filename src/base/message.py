8731339b4287d5aaf3d22f34f0d1feea4654a15d
aminbeigi <me@aminbeigi.com>
2021-04-04 18:52:07 +1000

    Complete messages edit

--- src/base/message.py
+++ src/base/message.py
@@ -1,13 +1,27 @@
 """TODO"""
 import time
 from src.base.error import InputError, AccessError
-from src.base.helper import user_is_member, get_channel, get_current_user, user_is_dm_member, remove_message, user_is_Dream_owner, user_is_owner, get_message_channel_id
+from src.base.helper import user_is_member, get_channel, get_current_user, user_is_dm_member, remove_message, user_is_Dream_owner, user_is_owner, get_message_channel_id, edit_message
 from src.data.helper import store_message, store_message_dm, get_message_count
 from src.base.helper import create_message
 
 def message_send_v1(auth_user_id, channel_id, message):
+    """Send a message from authorised_user to the channel specified by channel_id.
+    Note: Each message should have it's own unique ID. I.E. No messages should share
+    an ID with another message, even if that other message is in a different channel.
 
-   # channel_data = get_channel(channel_id)
+    Arguments:
+        auth_user_id (int) - The user's id
+        channel_id (int) - The id of the channel the message was sent in
+        message (str) - The message contents
+
+    Exceptions:
+        InputError - Message is more than 1000 characters
+        AccessError - The authorised user has not joined the channel they are trying to post to
+
+    Return Value:
+        Returns message_id on successfully sending a message
+    """
 
     if len(message) > 1000:
         raise InputError("Message is more than 1000 characters")
@@ -36,7 +50,7 @@ def message_remove_v1(auth_user_id, message_id):
             - The authorised user is an owner of this channel (if it was sent to a channel) or the **Dreams**
 
     Return Value:
-        Returns empty dict on all conditions
+        Returns empty dict on successfully removing a message
     """
     channel_id = get_message_channel_id(message_id)
     channel = get_channel(channel_id)
@@ -64,10 +78,21 @@ def message_edit_v1(auth_user_id, message_id, message):
             - The authorised user is an owner of this channel (if it was sent to a channel) or the **Dreams**
 
     Return Value:
-        Returns empty dict on all conditions
+        Returns empty dict on successfully editing a message
     """
-    return {
-    }
+    channel_id = get_message_channel_id(message_id)
+    channel = get_channel(channel_id)
+
+    if not user_is_owner(channel, auth_user_id) and not user_is_Dream_owner(auth_user_id):
+        raise AccessError(f"Message with message_id {message_id} was sent by the authorised user making this request")
+
+    if len(message) > 1000:
+        raise InputError("Message is more than 1000 characters")
+
+    if not edit_message(message_id, message):
+        raise InputError(f"message_id {message_id} refers to a deleted message")
+
+    return {}
 
 def message_senddm_v1(auth_user_id, dm_id, message):
     if not get_current_user(auth_user_id):
