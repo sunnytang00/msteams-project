6c1791ee18dfe5324d133c042b8907ff5d28b8d1
Sunny <z5255035@ad.unsw.edu.au>
2021-04-06 02:58:30 +1000

    added error checking for user profile

--- http_tests/user_profile_http_test.py
+++ http_tests/user_profile_http_test.py
@@ -3,6 +3,7 @@ from json import loads
 from src.config import url
 from http_tests.helper import clear
 import urllib
+from src.routes.helper import get_new_session_id, encode_token
 @clear
 
 def test_setprofile_basic():
@@ -27,3 +28,26 @@ def test_setprofile_basic():
     data = user.json()
     assert data.get('user').get('user').get('name_first') == 'harry'
     assert data.get('user').get('user').get('name_last') == 'potter'
+
+@clear
+
+def test_input_error():
+    response = requests.post(url + 'auth/register/v2', json = {
+        'email' : 'harrypotter@gmail.com',
+        'password' : 'dumbledore',
+        'name_first' : 'harry',
+        'name_last' : 'potter'
+    })
+
+    data = response.json()
+    token = data.get('token')
+
+    u_id = 1
+
+    queryString = urllib.parse.urlencode({
+        'token' : token,
+        'u_id' : 2
+    })
+    user = requests.get(url + f'user/profile/v2?{queryString}')
+
+    assert user.status_code == 400
