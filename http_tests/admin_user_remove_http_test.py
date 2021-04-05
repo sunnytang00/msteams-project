07552ef321b395cf1b4d4fa0d9f4780113159608
Wang Zhang <z5191535@ad.unsw.edu.au>
2021-04-05 18:32:49 +0800

    added more test for admin_uesr_remove

--- http_tests/admin_user_remove_http_test.py
+++ http_tests/admin_user_remove_http_test.py
@@ -5,6 +5,26 @@ from http_tests.helper import clear, helper
 from urllib.parse import urlencode
 
 @clear
+def test_valid_input(helper):
+    user1 = helper.register_user(1)
+    user2 = helper.register_user(2)
+    
+    token1 = user1.json().get('token')
+    token2 = user2.json().get('token')
+    assert token1 and token2
+    u_id = user2.json().get('auth_user_id')
+
+    response = requests.delete(url + "admin/user/remove/v1", json ={
+        'token': token1,
+        'u_id': u_id
+    })
+    assert response.status_code == 200
+
+    users = requests.get(url + "users/all/v1?token=" + token1).json()
+
+    assert u_id not in [user['u_id'] for user in users['users']]
+
+@clear
 def test_invalid_u_id(helper):
     user1 = helper.register_user(1)
 
@@ -78,9 +98,8 @@ def test_remove_only_member_of_channel(helper):
     assert u_id not in [user['u_id'] for user in channel['owner_members']] \
             and u_id not in [user['u_id'] for user in channel['all_members']]
 
+#function below needs message_senddm
 '''
-@clear
-def test_valid_input():
 
 @clear
 def test_removed_user_dm_msg(helper):
