ec5786da33f2c93e251a0aae6ed642f895a118e4
Sunny Tang <sunny.tang@student.unsw.edu.au>
2021-04-06 02:05:17 +1000

    Add error checking dm routes

--- http_tests/dm_create_http_test.py
+++ http_tests/dm_create_http_test.py
@@ -30,5 +30,23 @@ def test_basic(helper):
     assert dm_info.get('dm_id') == 1
     assert dm_info.get('dm_name') == get_dm_name([u_id, u_id2, u_id3])
 
+@clear
 
+def test_input_error(helper):
+
+    response = helper.register_user(1)
+    response3 = helper.register_user(3)
+
+    data = response.json()
+    data3 = response3.json()
+
+    token = data.get('token')
+    u_id3 = data3.get('auth_user_id')
+
+    dm = requests.post(url + 'dm/create/v1', json = {
+        'token' : token,
+        'u_ids' : [4, u_id3]
+    })
+    
+    assert dm.status_code == 400
 
--- http_tests/dm_details_http_test.py
+++ http_tests/dm_details_http_test.py
@@ -64,4 +64,73 @@ def test_basic(helper):
                                         'handle_str': 'cadifinch', 
                                         'permission_id': 2}]
 
+@clear
+def test_input_error(helper):
+
+    response = helper.register_user(1)
+    response2 = helper.register_user(2)
+    response3 = helper.register_user(3)
+
+    data = response.json()
+    data2 = response2.json()
+    data3 = response3.json()
+
+    token = data.get('token')
+    token2 = data2.get('token')
+
+    u_id2 = data2.get('auth_user_id')
+    assert u_id2 == 2
+    u_id3 = data3.get('auth_user_id')
+    assert u_id3 == 3
+
+    requests.post(url + 'dm/create/v1', json = {
+        'token' : token,
+        'u_ids' : [u_id2, u_id3]
+    })
+
+    queryString = urllib.parse.urlencode({
+        'token' : token2,
+        'dm_id' : 5
+    })
+  
+    dm = requests.get(url + f'dm/details/v1?{queryString}')
+
+    assert dm.status_code == 400
+
+@clear
+def test_access_error(helper):
+
+    response = helper.register_user(1)
+    response2 = helper.register_user(2)
+    response3 = helper.register_user(3)
+
+    data = response.json()
+    data2 = response2.json()
+    data3 = response3.json()
+
+    token = data.get('token')
+    token3 = data3.get('token')
+
+    u_id2 = data2.get('auth_user_id')
+    assert u_id2 == 2
+    u_id3 = data3.get('auth_user_id')
+    assert u_id3 == 3
+
+    dm_info = requests.post(url + 'dm/create/v1', json = {
+        'token' : token,
+        'u_ids' : [u_id2]
+    })
+
+    readable_dm = dm_info.json()
+    dm_id = readable_dm.get('dm_id')
+
+    queryString = urllib.parse.urlencode({
+        'token' : token3,
+        'dm_id' : dm_id
+    })
+  
+    dm = requests.get(url + f'dm/details/v1?{queryString}')
+
+    assert dm.status_code == 403
+
 
