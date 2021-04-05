e9cf638323ef4d00e6a0f06c05a886422ee5c239
Sunny <z5255035@ad.unsw.edu.au>
2021-04-06 03:21:22 +1000

    added more error checking

--- http_tests/user_profile_setemail_http_test.py
+++ http_tests/user_profile_setemail_http_test.py
@@ -31,3 +31,64 @@ def test_setemail_basic():
 
     data = user.json()
     assert data.get('user').get('user').get('email') == 'albusdumbledore@gmail.com'
+
+@clear
+
+def test_input_error():
+    response = requests.post(url + 'auth/register/v2', json = {
+        'email' : 'harrypotter@gmail.com',
+        'password' : 'dumbledore',
+        'name_first' : 'harry',
+        'name_last' : 'potter'
+    })
+
+    data = response.json()
+    token = data.get('token')
+
+    response = requests.put(url + 'user/profile/setemail/v2', json = {
+        'token' : token,
+        'email' : 'invalidemail!!!!!!!!',
+    })
+
+    assert response.status_code == 400
+
+@clear
+
+def test_input_error2():
+    response = requests.post(url + 'auth/register/v2', json = {
+        'email' : 'harrypotter@gmail.com',
+        'password' : 'dumbledore',
+        'name_first' : 'harry',
+        'name_last' : 'potter'
+    })
+
+    data = response.json()
+    u_id = data.get('auth_user_id')
+
+    response2 = requests.post(url + 'auth/register/v2', json = {
+        'email' : 'jamespotter@gmail.com',
+        'password' : 'severussnape',
+        'name_first' : 'james',
+        'name_last' : 'potter'
+    })
+    data2 = response2.json()
+    token2 = data2.get('token')
+    token = data.get('token')
+
+    
+    queryString = urllib.parse.urlencode({
+        'token' : token,
+        'u_id' : u_id
+    })
+    user = requests.get(url + f'user/profile/v2?{queryString}')
+    data = user.json()
+    email = data.get('user').get('user').get('email')
+
+
+    response = requests.put(url + 'user/profile/setemail/v2', json = {
+        'token' : token2,
+        'email' : email,
+    })
+
+    assert response.status_code == 400
+
