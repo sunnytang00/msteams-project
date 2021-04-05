07e78e27461b41b51cc98489412399e7e4235309
aminbeigi <me@aminbeigi.com>
2021-04-05 17:59:30 +1000

    Expand tests to edit DMs also

--- tests/message/message_edit_test.py
+++ tests/message/message_edit_test.py
@@ -19,7 +19,7 @@ def test_message_edit_single_message(helper):
     assert channel_id == 1
 
     og_message = "an epic message"
-    edited_message = "an even more epic message"
+    new_message = "an even more epic message"
 
     message_info = message_send_v1(auth_user_id, channel_id, og_message)
     message_id = message_info.get('message_id')
@@ -28,16 +28,30 @@ def test_message_edit_single_message(helper):
     messages = channel_messages_v1(auth_user_id, channel_id, 1).get('messages')
 
     assert messages[0].get('message') == og_message
-    message_edit_v1(auth_user_id, message_id, edited_message)
+    message_edit_v1(auth_user_id, message_id, new_message)
 
-    # get new updated message
-    messages = channel_messages_v1(auth_user_id, channel_id, 1).get('messages')
-    assert messages[0].get('message') == edited_message
+    messages = channel_messages_v1(auth_user_id, channel_id, 1).get('messages') # get new updated messages
+    assert messages[0].get('message') == new_message
+
+    # edit a message in DM
+    auth_user_id2 = helper.register_user(2)
+    assert auth_user_id2 == 2
+    u_ids = [auth_user_id2]
+    dm_id = dm_create_v1(auth_user_id, u_ids).get('dm_id')
+    assert dm_id == 1
+
+    message_info = message_senddm_v1(auth_user_id, dm_id, og_message)
+    message_id = message_info.get('message_id')
+    assert message_id == 2
+
+    message_edit_v1(auth_user_id, message_id, new_message)
+
+    messages = dm_messages_v1(auth_user_id, dm_id, 1).get('messages') # get new updated messages
+    assert messages[0].get('message') == new_message
 
 @clear
 def test_edit_deleted_message(helper):
     """try editting a message that doesn't exist"""
-
     # edit a message in channel
     auth_user_id = helper.register_user(1)
     assert auth_user_id == 1
@@ -60,7 +74,7 @@ def test_edit_deleted_message(helper):
     # edit a message in DM
     auth_user_id2 = helper.register_user(2)
     assert auth_user_id2 == 2
-    u_ids = [auth_user_id, auth_user_id2]
+    u_ids = [auth_user_id2]
     dm_id = dm_create_v1(auth_user_id, u_ids).get('dm_id')
     assert dm_id == 1
 
@@ -77,6 +91,7 @@ def test_edit_deleted_message(helper):
 @clear
 def test_message_over_1000_char(helper):
     """try editting a message that is over 1000 characters"""
+    # edit a message in channel
     auth_user_id = helper.register_user(1)
     assert auth_user_id == 1
 
@@ -84,14 +99,29 @@ def test_message_over_1000_char(helper):
     assert channel_id == 1
 
     og_message = "trimesters are super awesome"
-    editted_message = "a"*1001
+    new_message = "a"*1001
 
     message_info = message_send_v1(auth_user_id, channel_id, og_message)
     message_id = message_info.get('message_id')
     assert message_id == 1
 
     with pytest.raises(InputError) as e: 
-        message_edit_v1(auth_user_id, message_id, editted_message)
+        message_edit_v1(auth_user_id, message_id, new_message)
+        assert f"Length of message is over 1000 characters" in str(e.value)
+
+    # edit a message in DM
+    auth_user_id2 = helper.register_user(2)
+    assert auth_user_id2 == 2
+    u_ids = [auth_user_id2]
+    dm_id = dm_create_v1(auth_user_id, u_ids).get('dm_id')
+    assert dm_id == 1
+
+    message_info = message_senddm_v1(auth_user_id, dm_id, og_message)
+    message_id = message_info.get('message_id')
+    assert message_id == 2
+
+    with pytest.raises(InputError) as e: 
+        message_edit_v1(auth_user_id, message_id, new_message)
         assert f"Length of message is over 1000 characters" in str(e.value)
 
 @clear
@@ -109,8 +139,27 @@ def test_user_is_authorised(helper):
     message_id = message_info.get('message_id') 
     assert message_id == 1
 
-    message = "the batmobile"
+    og_message = "the batmobile"
+    new_message = "trucks are cool just like bikes"
 
     with pytest.raises(AccessError) as e: 
-        message_edit_v1(not_auth_user_id, message_id, message)
+        message_edit_v1(not_auth_user_id, message_id, og_message)
+        assert f"Message with message_id {message_id} was not sent by the authorised user making this request" in str(e.value)
+
+    # edit a message in DM
+    u_ids = [not_auth_user_id] # even though a member shouldn't be able to edit message of another user in same DM
+    dm_id = dm_create_v1(auth_user_id, u_ids).get('dm_id')
+    assert dm_id == 1
+
+    message_info = message_senddm_v1(auth_user_id, dm_id, og_message)
+    message_id = message_info.get('message_id')
+    assert message_id == 2
+
+    message_edit_v1(auth_user_id, message_id, new_message)
+
+    messages = dm_messages_v1(auth_user_id, dm_id, 1).get('messages') # get new updated messages
+    assert messages[0].get('message') == new_message
+
+    with pytest.raises(AccessError) as e:
+        message_edit_v1(not_auth_user_id, message_id, new_message)
         assert f"Message with message_id {message_id} was not sent by the authorised user making this request" in str(e.value)
