7ca90e5d2aefb15b278fedaeb10f9db6be48fd13
Wang Zhang <z5191535@ad.unsw.edu.au>
2021-04-05 17:41:29 +0800

    added route for admin_user_remove

--- src/routes/admin_http.py
+++ src/routes/admin_http.py
@@ -2,7 +2,7 @@ import sys
 from json import dumps
 from flask import Flask, request, Blueprint
 from src.base.other import clear_v1
-from src.base.admin import admin_userpermission_change_v1
+from src.base.admin import admin_userpermission_change_v1, admin_user_remove_v1
 from src.base.helper import token_to_auth_user_id
 
 admin_blueprint = Blueprint('admin_blueprint', __name__)
@@ -16,5 +16,16 @@ def admin_userpermission_change():
 
     auth_user_id = token_to_auth_user_id(token)
     admin_userpermission_change_v1(auth_user_id, u_id, permission_id)
+
+    return dumps({}), 200
+
+@admin_blueprint.route("/admin/user/remove/v1", methods=['DELETE'])
+def admin_user_remove():
+    data = request.get_json()
+    token = data.get('token')
+    u_id = data.get('u_id')
+
+    auth_user_id = token_to_auth_user_id(token)
+    admin_user_remove_v1(auth_user_id, u_id)
 
     return dumps({}), 200
