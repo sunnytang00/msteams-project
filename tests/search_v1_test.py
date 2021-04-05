f2da91201838bb79c2b121915025f681352f5ab0
z5223352 <z5223352@cse.unsw.edu.au>
2021-04-05 19:18:09 +1000

    removed unused variables in search tests

--- tests/search_v1_test.py
+++ tests/search_v1_test.py
@@ -10,7 +10,7 @@ from src.base.channels import channels_create_v1
 def test_empty_query_str():
     user = auth_register_v1('bobsmith2@gmail.com','12345678','Bob','Smith')
     user_id = user['auth_user_id']
-    
+
     with pytest.raises(InputError) as e:
         search_v1(user_id, "")
         assert 'Query string is empty' in str(e.value)
@@ -74,7 +74,7 @@ def test_not_in_a_channel():
     test_channel = channels_create_v1(user_id, "correct", True)
     channel_id = test_channel['channel_id']
 
-    test_channel_1 = channels_create_v1(user_id_1, "correct", True)
+    channels_create_v1(user_id_1, "correct", True)
 
     message_send_v1(user_id, channel_id, "Hello")
 
