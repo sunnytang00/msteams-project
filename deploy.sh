5c4dfc6ce6632aedf4c0ed7fc567ce1d58d096c0
aminbeigi <me@aminbeigi.com>
2021-04-14 10:50:58 +1000

    Add username and hostname to deploy

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 
 WORKING_DIRECTORY="~/www/cs1531deploy"
 
-USERNAME="TODO-TODO-TODO"
-SSH_HOST="TODO-TODO-TODO"
+USERNAME="fri09becho"
+SSH_HOST="ssh-fri09becho.alwaysdata.net"
 
 rm -rf ./**/__pycache__ ./**/.pytest_cache > /dev/null
 scp -r ./requirements.txt ./src "$USERNAME@$SSH_HOST:$WORKING_DIRECTORY"
