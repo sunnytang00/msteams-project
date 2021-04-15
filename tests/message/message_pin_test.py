af92e6e0333bd6e92aa69f2359936d66de171fb5
Sunny <z5255035@ad.unsw.edu.au>
2021-04-15 15:36:28 +1000

    added more test cases to test error cases

--- tests/message/message_pin_test.py
+++ tests/message/message_pin_test.py
@@ -56,4 +56,55 @@ def test_pin_single_message_dm(helper):
     message_pin_v1(auth_user_id1, message_id2)
     assert is_pinned(message_id1) == False
     assert is_pinned(message_id2) == True
+
+@clear
+def test_pin_invalid_message_id(helper):
+    """try to pin invalid message id"""
+    auth_user_id1 = helper.register_user(1)
+    auth_user_id2 = helper.register_user(2)
+    assert auth_user_id1 == 1
+    assert auth_user_id2 == 2
+    
+    dm_id = dm_create_v1(auth_user_id1, [auth_user_id2]).get('dm_id')
+    assert dm_id == 1
+
+    first_message = "this shouldnt be pinned"
+    second_message = "hopefully this one gets pinned"
+
+    message_id1 = message_senddm_v1(auth_user_id2, dm_id, first_message).get('message_id')
+    assert message_id1 == 1
+
+    message_id2 = message_senddm_v1(auth_user_id2, dm_id, second_message).get('message_id')
+    assert message_id2 == 2
+
+    with pytest.raises(InputError) as e: 
+        message_pin_v1(auth_user_id1, 22)
+        assert f'message with message id {message_id} is not a valid message' in str(e.value)
+
+@clear
+def test__pin_pinned_message(helper):
+    """try to pin already pinned message"""
+    auth_user_id1 = helper.register_user(1)
+    auth_user_id2 = helper.register_user(2)
+    assert auth_user_id1 == 1
+    assert auth_user_id2 == 2
+    
+    dm_id = dm_create_v1(auth_user_id1, [auth_user_id2]).get('dm_id')
+    assert dm_id == 1
+
+    first_message = "this shouldnt be pinned"
+    second_message = "hopefully this one gets pinned"
+
+    message_id1 = message_senddm_v1(auth_user_id2, dm_id, first_message).get('message_id')
+    assert message_id1 == 1
+
+    message_id2 = message_senddm_v1(auth_user_id2, dm_id, second_message).get('message_id')
+    assert message_id2 == 2
+
+    message_pin_v1(auth_user_id1, message_id2)
+    assert is_pinned(message_id1) == False
+    assert is_pinned(message_id2) == True
+    with pytest.raises(InputError) as e: 
+        message_pin_v1(auth_user_id1, message_id2)
+        assert f'message with message id {message_id} is already pinned' in str(e.value)
     
