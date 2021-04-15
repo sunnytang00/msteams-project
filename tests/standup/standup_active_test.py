aa33e053995a1eba7e88c5294c1f89236c7d8c52
Wang Zhang <z5191535@ad.unsw.edu.au>
2021-04-15 17:30:18 +0800

    added test for standup_active

---
+++ tests/standup/standup_active_test.py
@@ -0,0 +1,36 @@
+import pytest
+import time
+from src.base.channels import channels_create_v1
+from src.base.channel import channel_details_v1, channel_addowner_v1, channel_join_v1
+from src.base.error import InputError, AccessError
+from src.base.standup import standup_send_v1, standup_start_v1, standup_active_v1
+from src.base.other import clear_v1
+from tests.helper import helper, clear
+
+@clear
+def test_valid_input(helper):
+    auth_user_id = helper.register_user(1)
+    ch_id = helper.create_channel(1, auth_user_id)
+    length = 10
+    time_finish = standup_start_v1(auth_user_id, ch_id, length)
+    standup_data = standup_active_v1(auth_user_id, ch_id)
+    assert standup_data.get('is_active') == True and standup_data.get('time_finish') == time_finish
+
+@clear
+def test_invalid_channel(helper):
+    auth_user_id = helper.register_user(1)
+    ch_id = 10 
+    with pytest.raises(InputError) as e: 
+        standup_active_v1(auth_user_id, ch_id)
+        assert f'Channel ID {ch_id} is not a valid channel' in str(e.value)
+
+@clear
+def test_invalid_token(helper):
+    auth_user_id = helper.register_user(1)
+    ch_id = helper.create_channel(1, auth_user_id)
+    length = 10
+    with pytest.raises(AccessError) as e: 
+        standup_active_v1(auth_user_id, ch_id)
+        assert f'token {auth_user_id} does not refer to a valid token' in str(e.value)
+
+
