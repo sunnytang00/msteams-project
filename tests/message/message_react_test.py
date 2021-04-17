b94a57aed9c73854a51d184b93cbe0696a660a1c
Sunny <z5255035@ad.unsw.edu.au>
2021-04-17 18:38:25 +1000

    added tests for unreacting

--- tests/message/message_react_test.py
+++ tests/message/message_react_test.py
@@ -1,5 +1,5 @@
 import pytest
-from src.base.message import message_send_v1, message_senddm_v1, message_remove_v1, message_edit_v1, message_react_v1
+from src.base.message import message_send_v1, message_senddm_v1, message_remove_v1, message_edit_v1, message_react_v1, message_unreact_v1
 from src.base.auth import auth_register_v1
 from src.base.dm import dm_create_v1, dm_messages_v1, dm_invite_v1
 from src.base.channel import channel_messages_v1, channel_invite_v1, channel_join_v1, channel_addowner_v1
@@ -13,7 +13,6 @@ valid_react_id = 1
 invalid_react_id = 2
 @clear
 def test_react_single_message_channel(helper):
-    """Add two messages and pin the second one"""
     auth_user_id1 = helper.register_user(1)
     auth_user_id2 = helper.register_user(2)
     assert auth_user_id1 == 1
@@ -43,7 +42,6 @@ def test_react_single_message_channel(helper):
 
 @clear
 def test_react_single_message_dm(helper):
-    """Add two messages and pin the second one"""
     auth_user_id1 = helper.register_user(1)
     auth_user_id2 = helper.register_user(2)
     assert auth_user_id1 == 1
@@ -71,8 +69,7 @@ def test_react_single_message_dm(helper):
     assert get_react_uids(message_id2) == [1,2]
 
 @clear
-def test_invalid_react_id(helper):
-    """Add two messages and pin the second one"""
+def test_invalid_react_unreact_id(helper):
     auth_user_id1 = helper.register_user(1)
     auth_user_id2 = helper.register_user(2)
     assert auth_user_id1 == 1
@@ -109,7 +106,15 @@ def test_invalid_react_id(helper):
         assert f'react_id {invalid_react_id} is not a valid React ID' in str(e.value)
 
     with pytest.raises(InputError) as e: 
+        message_unreact_v1(auth_user_id1, message_id1, invalid_react_id)
+        assert f'react_id {invalid_react_id} is not a valid React ID' in str(e.value)
+
+    with pytest.raises(InputError) as e: 
         message_react_v1(auth_user_id1, message_id2, invalid_react_id)
+        assert f'react_id {invalid_react_id} is not a valid React ID' in str(e.value)
+
+    with pytest.raises(InputError) as e: 
+        message_unreact_v1(auth_user_id1, message_id2, invalid_react_id)
         assert f'react_id {invalid_react_id} is not a valid React ID' in str(e.value)
 
     with pytest.raises(InputError) as e: 
@@ -117,7 +122,15 @@ def test_invalid_react_id(helper):
         assert f'react_id {invalid_react_id} is not a valid React ID' in str(e.value)
 
     with pytest.raises(InputError) as e: 
+        message_unreact_v1(auth_user_id2, message_id1, invalid_react_id)
+        assert f'react_id {invalid_react_id} is not a valid React ID' in str(e.value)
+
+    with pytest.raises(InputError) as e: 
         message_react_v1(auth_user_id2, message_id2, invalid_react_id)
+        assert f'react_id {invalid_react_id} is not a valid React ID' in str(e.value)
+
+    with pytest.raises(InputError) as e: 
+        message_unreact_v1(auth_user_id2, message_id2, invalid_react_id)
         assert f'react_id {invalid_react_id} is not a valid React ID' in str(e.value)
 
     with pytest.raises(InputError) as e: 
@@ -125,7 +138,15 @@ def test_invalid_react_id(helper):
         assert f'react_id {invalid_react_id} is not a valid React ID' in str(e.value)
 
     with pytest.raises(InputError) as e: 
+        message_unreact_v1(auth_user_id1, message_id3, invalid_react_id)
+        assert f'react_id {invalid_react_id} is not a valid React ID' in str(e.value)
+
+    with pytest.raises(InputError) as e: 
         message_react_v1(auth_user_id1, message_id4, invalid_react_id)
+        assert f'react_id {invalid_react_id} is not a valid React ID' in str(e.value)
+
+    with pytest.raises(InputError) as e: 
+        message_unreact_v1(auth_user_id1, message_id4, invalid_react_id)
         assert f'react_id {invalid_react_id} is not a valid React ID' in str(e.value)
 
     with pytest.raises(InputError) as e: 
@@ -133,7 +154,15 @@ def test_invalid_react_id(helper):
         assert f'react_id {invalid_react_id} is not a valid React ID' in str(e.value)
 
     with pytest.raises(InputError) as e: 
+        message_unreact_v1(auth_user_id2, message_id3, invalid_react_id)
+        assert f'react_id {invalid_react_id} is not a valid React ID' in str(e.value)
+
+    with pytest.raises(InputError) as e: 
         message_react_v1(auth_user_id2, message_id4, invalid_react_id)
+        assert f'react_id {invalid_react_id} is not a valid React ID' in str(e.value)
+
+    with pytest.raises(InputError) as e: 
+        message_unreact_v1(auth_user_id2, message_id4, invalid_react_id)
         assert f'react_id {invalid_react_id} is not a valid React ID' in str(e.value)
 
     assert get_react_uids(message_id1) == []
@@ -144,7 +173,6 @@ def test_invalid_react_id(helper):
 
 @clear
 def test_react_to_invalid_msg_id(helper):
-    """Add two messages and pin the second one"""
     auth_user_id1 = helper.register_user(1)
     auth_user_id2 = helper.register_user(2)
     assert auth_user_id1 == 1
@@ -166,9 +194,16 @@ def test_react_to_invalid_msg_id(helper):
         message_react_v1(auth_user_id2, invalid_message_id, valid_react_id)
         assert f'message_id {invalid_message_id} is not a valid message within a channel/dm' in str(e.value)
 
+    with pytest.raises(InputError) as e: 
+        message_unreact_v1(auth_user_id1, invalid_message_id, valid_react_id)
+        assert f'message_id {invalid_message_id} is not a valid message within a channel/dm' in str(e.value)
+
+    with pytest.raises(InputError) as e: 
+        message_unreact_v1(auth_user_id2, invalid_message_id, valid_react_id)
+        assert f'message_id {invalid_message_id} is not a valid message within a channel/dm' in str(e.value)
+
 @clear
 def test_non_member_react_and_react_reacted(helper):
-    """Add two messages and pin the second one"""
     auth_user_id1 = helper.register_user(1)
     auth_user_id2 = helper.register_user(2)
     auth_user_id3 = helper.register_user(3)
@@ -197,6 +232,10 @@ def test_non_member_react_and_react_reacted(helper):
         message_react_v1(auth_user_id3, message_id1, valid_react_id)
         assert f'member with id {auth_user_id3} is not dm member' in str(e.value)
 
+    with pytest.raises(AccessError) as e: 
+        message_unreact_v1(auth_user_id3, message_id1, valid_react_id)
+        assert f'member with id {auth_user_id3} is not dm member' in str(e.value)
+
     assert get_react_uids(message_id1) == []
 
     dm_invite_v1(auth_user_id2, dm_id, auth_user_id3)
@@ -211,6 +250,10 @@ def test_non_member_react_and_react_reacted(helper):
         message_react_v1(auth_user_id3, message_id2, valid_react_id)
         assert f'member with id {auth_user_id3} is not channel member' in str(e.value)
 
+    with pytest.raises(AccessError) as e: 
+        message_unreact_v1(auth_user_id3, message_id2, valid_react_id)
+        assert f'member with id {auth_user_id3} is not channel member' in str(e.value)
+
     channel_invite_v1(auth_user_id2, channel_id, auth_user_id3)
     message_react_v1(auth_user_id3, message_id2, valid_react_id)
 
@@ -220,3 +263,87 @@ def test_non_member_react_and_react_reacted(helper):
         message_react_v1(auth_user_id3, message_id2, valid_react_id)
         assert f'user with id {auth_user_id3} has already reacted to message id {message_id2} in channel' in str(e.value)
 
+@clear
+def test_unreact_single_message_dm(helper):
+    auth_user_id1 = helper.register_user(1)
+    auth_user_id2 = helper.register_user(2)
+    assert auth_user_id1 == 1
+    assert auth_user_id2 == 2
+    dm_id = dm_create_v1(auth_user_id1, [auth_user_id2]).get('dm_id')
+    assert dm_id == 1
+
+    first_message = "this shouldnt have reacts"
+    second_message = "hopefully this one does"
+
+    message_info1 = message_senddm_v1(auth_user_id1, dm_id, first_message)
+    message_id1 = message_info1.get('message_id')
+    assert message_id1 == 1
+
+    message_info2 = message_senddm_v1(auth_user_id1, dm_id, second_message)
+    message_id2 = message_info2.get('message_id')
+    assert message_id2 == 2
+
+    assert get_react_uids(message_id2) == []
+
+    message_react_v1(auth_user_id1, message_id2, valid_react_id)
+
+    assert get_react_uids(message_id2) == [1]
+
+    message_react_v1(auth_user_id2, message_id2, valid_react_id)
+
+    assert get_react_uids(message_id2) == [1,2]
+
+    message_unreact_v1(auth_user_id2, message_id2, valid_react_id)
+    message_unreact_v1(auth_user_id1, message_id2, valid_react_id)
+
+    assert get_react_uids(message_id2) == []
+
+    with pytest.raises(InputError) as e: 
+        message_unreact_v1(auth_user_id1, message_id2, valid_react_id)
+        assert f'user with id {auth_user_id1} has no active reacts for message id {message_id2} in dm' in str(e.value)
+
+    with pytest.raises(InputError) as e: 
+        message_unreact_v1(auth_user_id2, message_id2, valid_react_id)
+        assert f'user with id {auth_user_id2} has no active reacts for message id {message_id2} in dm' in str(e.value)
+
+@clear
+def test_unreact_single_message_channel(helper):
+    auth_user_id1 = helper.register_user(1)
+    auth_user_id2 = helper.register_user(2)
+    assert auth_user_id1 == 1
+    assert auth_user_id2 == 2
+    channel_id = channels_create_v1(auth_user_id1, "message_test", True).get('channel_id')
+    assert channel_id == 1
+    channel_invite_v1(auth_user_id1, channel_id, auth_user_id2)
+
+    first_message = "this shouldnt have reacts"
+    second_message = "hopefully this one does"
+
+    message_info1 = message_send_v1(auth_user_id1, channel_id, first_message)
+    message_id1 = message_info1.get('message_id')
+    assert message_id1 == 1
+
+    message_info2 = message_send_v1(auth_user_id1, channel_id, second_message)
+    message_id2 = message_info2.get('message_id')
+    assert message_id2 == 2
+
+    message_react_v1(auth_user_id1, message_id2, valid_react_id)
+
+    assert get_react_uids(message_id2) == [1]
+
+    message_react_v1(auth_user_id2, message_id2, valid_react_id)
+
+    assert get_react_uids(message_id2) == [1,2]
+
+    message_unreact_v1(auth_user_id2, message_id2, valid_react_id)
+    message_unreact_v1(auth_user_id1, message_id2, valid_react_id)
+
+    assert get_react_uids(message_id2) == []
+
+    with pytest.raises(InputError) as e: 
+        message_unreact_v1(auth_user_id1, message_id2, valid_react_id)
+        assert f'user with id {auth_user_id1} has no active reacts for message id {message_id2} in channel' in str(e.value)
+
+    with pytest.raises(InputError) as e: 
+        message_unreact_v1(auth_user_id2, message_id2, valid_react_id)
+        assert f'user with id {auth_user_id2} has no active reacts for message id {message_id2} in channel' in str(e.value)
