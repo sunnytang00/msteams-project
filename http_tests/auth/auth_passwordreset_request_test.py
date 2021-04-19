cf4d45332bef8ee8f2d76e858c983f9a4934aa95
aminbeigi <me@aminbeigi.com>
2021-04-19 22:53:57 +1000

    Add basic password request test

---
+++ http_tests/auth/auth_passwordreset_request_test.py
@@ -0,0 +1,14 @@
+import requests
+from json import loads
+from src.config import url
+from http_tests.helper import clear, helper
+
+@clear
+def test_send_email(helper):
+    email = "georgebush24@gmail.com"
+    helper.register_user(1, email=email)
+
+    response = requests.post(url + 'auth/passwordreset/request/v1', json = {
+        'email': email
+    })
+    assert response.status_code == 200
